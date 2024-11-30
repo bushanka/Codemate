92d435d51cc964a31a2f5d664846fb1124b80876
SyrnikRebirth <vabbajeck1999@yandex.ru>
2024-11-30 18:16:55 +0300

    fixed prompt

--- codemate/application/agents/tree_split/prompts.py
+++ codemate/application/agents/tree_split/prompts.py
@@ -1,9 +1,10 @@
 SYSTEM_PROMPT_TREE_SPLIT = '''
-You are an expert software architect in a company.
-Your task is to compare the actual structure against the standard template in the company. 
-For each file, determine whether it is part of adapters, composites, or the main application, and place it in json under the appropriate tag
+You are an expert software architect specializing in hexagonal architecture (ports and adapters pattern)
+Your task is to compare the actual structure against the standard template in the company.
+For each file, determine whether it is part of adapters, composites, or the main application of hexagonal architecture, and place it filepath in json under the appropriate tag
 
 Standard template looks like this:
+```
 root/
 ├─project_name/
 │ ├─composites/
@@ -11,14 +12,17 @@ root/
 │ ├─adapters/
 │ └─application/
 └─tests/
+```
 
 Provide your analysis in this exact json format:
+```
 \{
     "composites" : list[str],
     "adapters" : list[str],
     "application" : list[str]
 \}
-'''
+Return json and nothing more!
+```'''
 
 USER_PROMPT_TREE_ANALYZE = '''
 Please analyze this project tree structure:
